95082991c08e198e726e8841802cc59729e08b01
Muhammad Jawwad Khan <jawwadkhan0931@gmail.com>
2023-11-15 13:44:43 -0800

    New Update

--- Admin/AppointLawyer.php
+++ Admin/AppointLawyer.php
@@ -1,80 +1,66 @@
 <?php
-    session_start();
-    include('Authentication.php');
-    include('Config/db.php');
-    include('includes/header.php');
-    include('includes/sidebar.php');
-    include('includes/topbar.php');
+session_start();
+include('Authentication.php');
+include('Config/db.php');
+include('includes/header.php');
+include('includes/sidebar.php');
+include('includes/topbar.php');
 ?>
-
 <!-- Content Wrapper -->
 <div class="content-wrapper">
- <!-- Table -->
- <div class="container mt-5">
+  <!-- Table -->
+  <div class="container mt-5">
     <div class="row">
       <div class="col-12">
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Registration table</h3>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body">
-            <table id="example1" class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>FullName</th>
-                    <th>Services</th>
-                    <th>AppointDate</th>
-                </tr>
-              </thead>
-              <tbody>
-                <!-- Fetch all the data from database -->
-                <?php 
-                include("Config/db.php");
-                if($conn){
-                $Lawyerslt = "SELECT * FROM `appointlawyer`";;
+        <div class="card bg-light  mt-2 mb-2">
+          <h4 class="card-title pt-3 pb-3 ml-2">Registration table</h4>
+        </div>
+        <div class="table-responsive">
+          <table class="table table-dark table-striped table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>FullName</th>
+                <th>Services</th>
+                <th>AppointDate</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- Fetch all the data from database -->
+              <?php
+              include("Config/db.php");
+              if ($conn) {
+                $Lawyerslt = "SELECT * FROM `appointlawyer`";
+                ;
                 $Run_detailsql = mysqli_query($conn, $Lawyerslt);
 
                 // Check if any rows were returned
                 if (mysqli_num_rows($Run_detailsql) > 0) {
-                    echo '<table class="table">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th>Id</th>
-                            <th>FullName</th>
-                            <th>Services</th>
-                            <th>AppointDate</th>
-                        </tr>
-                    </thead>
-                    <tbody>';
-
-                    // Fetch data and output rows in the table
-                    while ($row = mysqli_fetch_assoc($Run_detailsql)) {
-                        echo '<tr>
-                        <td scope="row"> '. $row['Id'] .'</td>
-                        <td scope="row"> '. $row['FullName'] .'</td>
-                        <td scope="row"> '. $row['Serivce'] .'</td> 
-                        <td scope="row"> '. $row['AppointDate'] .'</td> 
+                  // Fetch data and output rows in the table
+                  while ($row = mysqli_fetch_assoc($Run_detailsql)) {
+                    echo '<tr>
+                        <td scope="row"> ' . $row['Id'] . '</td>
+                        <td scope="row"> ' . $row['FullName'] . '</td>
+                        <td scope="row"> ' . $row['Serivce'] . '</td> 
+                        <td scope="row"> ' . $row['AppointDate'] . '</td> 
                         </tr>';
-                    }
+                  }
 
-                    echo '</tbody>
+                  echo '</tbody>
                     </table>';
                 } else {
-                    echo "No records found";
+                  echo "No records found";
                 }
 
                 // Close the connection
                 $conn->close();
-            }
-            ?>
-              <tbody>
-            </table>
-          </div>
-          <?php include('includes/footer.php'); ?>
+              }
+              ?>
+            <tbody>
+          </table>
         </div>
       </div>
     </div>
   </div>
 </div>
+<?php include('includes/footer.php'); ?>
--- Admin/Register.php
+++ Admin/Register.php
@@ -16,15 +16,12 @@ include('includes/topbar.php');
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Register user</li>
-            <li class="breadcrumb-item active">Register</li>
+            <li class="breadcrumb-item"><a href="../index.php" class="text-dark">Back Home</a></li>
           </ol>
         </div>
       </div>
     </div>
   </div>
-
   <!-- Popup Modal use for Crud -->
   <div class="container">
     <!-- check Session -->
@@ -36,97 +33,95 @@ include('includes/topbar.php');
       }
       ?>
     </div>
-    <div class="row m-3">
-      <div class="col-6">
-        <h4>Register User</h4>
-      </div>
-      <div class="col-6  d-flex justify-content-end">
-        <!-- Modal -->
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-          Add User
-        </button>
-        <!-- Modal Form Content Here -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <form Action='code.php' method='POST'>
-                <div class="modal-body">
-                  <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name" name="Name" placeholder="Enter your name" required>
+    <div class="card bg-light">
+      <div class="row m-3">
+        <div class="col-6">
+          <h4>Register User</h4>
+        </div>
+        <div class="col-6  d-flex justify-content-end">
+          <!-- Modal -->
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-outline-dark" data-toggle="modal" data-target="#exampleModal">
+            Add User
+          </button>
+          <!-- Modal Form Content Here -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <form Action='code.php' method='POST'>
+                  <div class="modal-body">
+                    <div class="form-group">
+                      <label for="name">Name</label>
+                      <input type="text" class="form-control" id="name" name="Name" placeholder="Enter your name"
+                        required>
+                    </div>
+                    <div class="form-group">
+                      <label for="email">Email</label>
+                      <input type="email" class="form-control" id="email" name="Email" placeholder="Enter your email"
+                        required>
+                    </div>
+                    <div class="form-group">
+                      <label for="phone">Phone Number</label>
+                      <input type="tel" class="form-control" id="phone" name="Phone"
+                        placeholder="Enter your phone number" required>
+                    </div>
+                    <div class="row">
+                      <div class="col-sm-6">
+                        <div class="form-group">
+                          <label for="password">Password</label>
+                          <input type="password" class="form-control" name="Password" id="password"
+                            placeholder="Enter your password" required>
+                        </div>
+                      </div>
+                      <div class="col-sm-6">
+                        <div class="form-group">
+                          <label for="password">Confirm Password</label>
+                          <input type="password" class="form-control" name="Confirm-Password" id="password"
+                            placeholder="Enter your password" required>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" name="Email" placeholder="Enter your email"
-                      required>
-                  </div>
-                  <div class="form-group">
-                    <label for="phone">Phone Number</label>
-                    <input type="tel" class="form-control" id="phone" name="Phone"
-                      placeholder="Enter your phone number" required>
-                  </div>
-                  <div class="row">
-                  <div class="col-sm-6">
-                  <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" name="Password" id="password"
-                      placeholder="Enter your password" required>
-                  </div>
-                  </div>
-                  <div class="col-sm-6">
-                  <div class="form-group">
-                    <label for="password">Confirm Password</label>
-                    <input type="password" class="form-control" name="Confirm-Password" id="password"
-                      placeholder="Enter your password" required>
-                  </div>
-                  </div>
-                  </div>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="submit" class="btn btn-secondary" name='AddUser'>Submit</button>
-              </form>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-secondary" name='AddUser'>Submit</button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-
-
   <!-- Table -->
-  <div class="container">
+  <div class="container-fluid">
     <div class="row">
       <div class="col-12">
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Registration table</h3>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body">
-            <table id="example1" class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Phone Number</th>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                <!-- Fetch all the data from database -->
-                <?php 
-                   if($conn){
-                    $slt = "SELECT * FROM `user-table`";
-                    $query = mysqli_query($conn,$slt);
-                    if($query){
-                        while($row=mysqli_fetch_assoc($query)){
-                            $id = $row['Id'];
-                            $name = $row['Name'];
-                            $email = $row['Email'];
-                            $phone = $row['Phone'];
-                            echo "<tr>
+        <!-- /Table -->
+        <div class="table-responsive">
+          <table class="table table-dark table-striped table-bordered table-hover">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- Fetch all the data from database -->
+              <?php
+              if ($conn) {
+                $slt = "SELECT * FROM `user-table`";
+                $query = mysqli_query($conn, $slt);
+                if ($query) {
+                  while ($row = mysqli_fetch_assoc($query)) {
+                    $id = $row['Id'];
+                    $name = $row['Name'];
+                    $email = $row['Email'];
+                    $phone = $row['Phone'];
+                    echo "<tr>
                             <td>$name</td>
                             <td>$email</td>
                             <td>$phone </td>
@@ -135,17 +130,15 @@ include('includes/topbar.php');
                             <a href='delete.php?deleteid=$id' class='btn btn-danger'>Delete</a>
                             </td>
                             </tr>";
-                        }
-                    }
+                  }
                 }
-                ?>
-              <tbody>
-            </table>
-          </div>
-          <?php include('includes/footer.php'); ?>
+              }
+              ?>
+            <tbody>
+          </table>
         </div>
       </div>
     </div>
   </div>
-
 </div>
+<?php include('includes/footer.php'); ?>
