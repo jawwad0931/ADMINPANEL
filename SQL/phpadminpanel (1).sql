b7cd4568d88908b24f6c7dec9080a6b9e7a8e5d9
Muhammad Jawwad Khan <jawwadkhan0931@gmail.com>
2023-12-03 11:55:53 -0800

    SqlFileAdd

--- SQL/phpadminpanel (1).sql
+++ SQL/phpadminpanel (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2023 at 12:10 AM
+-- Generation Time: Dec 03, 2023 at 08:43 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,8 +41,12 @@ CREATE TABLE `appointlawyer` (
 
 INSERT INTO `appointlawyer` (`Id`, `FullName`, `Serivce`, `AppointDate`, `status`) VALUES
 (1, 'hakem', 'advocate', '2023-11-14', 1),
-(2, 'muhammad', 'Family', '2023-11-08', 0),
-(3, 'Malik khan', 'Advocate', '2023-11-14', 1);
+(2, 'muhammad', 'Family', '2023-11-08', 1),
+(3, 'Malik khan', 'Advocate', '2023-11-14', 1),
+(4, 'add', 'Family', '2023-12-12', 0),
+(5, 'muhammad', 'Environmental Law', '2023-12-07', 0),
+(6, 'add', 'Labor Law', '2023-12-27', 0),
+(7, 'javaid', 'Bussiness Law', '2023-12-12', 0);
 
 -- --------------------------------------------------------
 
@@ -65,11 +69,15 @@ CREATE TABLE `payment` (
 --
 
 INSERT INTO `payment` (`Id`, `Full_Name`, `Email`, `Lawyer_Name`, `City`, `PaymentMode`, `Payment_status`) VALUES
-(4, 'Ali', 'jawwad@gmail.com', ' Nawaz', 'city', 'Cash', 1),
-(5, '', '', ' ', '', '', 1),
-(6, 'Ali', 'jawwad@gmail.com', ' Nawaz', 'city', 'Cash', 0),
-(7, 'ali', 'ab@gmail.com', ' Nawaz', 'city', 'Cash', 0),
-(8, 'ali', 'ab@gmail.com', ' Nawaz', 'city', 'Cash', 0);
+(4, 'Ali', 'jawwad@gmail.com', ' Nawaz', 'city', 'Cash', 0),
+(9, 'Ali', 'jawwad@gmail.com', ' Nawaz', 'city', 'Cash', 0),
+(10, 'Ali', 'jawwad@gmail.com', ' Nawaz', 'city', 'Cash', 1),
+(12, 'Ali', 'jawwad@gmail.com', ' Nawaz', 'city', 'Cash', 0),
+(13, 'Ali', 'jawwad@gmail.com', ' Nawaz', 'city', 'Cash', 1),
+(17, 'Ali', 'jawwad@gmail.com', ' Nawaz', 'city', 'Cash', 1),
+(18, 'ali', 'ab@gmail.com', ' Nawaz', 'city', 'Cash', 0),
+(19, 'Ali', 'jawwadk638@gmail.com', ' Nawaz', 'city', 'Cash', 0),
+(20, 'Ali', 'jawwadk638@gmail.com', ' Nawaz', 'city', 'Cash', 0);
 
 -- --------------------------------------------------------
 
@@ -90,9 +98,9 @@ CREATE TABLE `services` (
 --
 
 INSERT INTO `services` (`Id`, `Image`, `Servicetype`, `LawyerDesc`, `fees`) VALUES
-(33, 'code-g32ca37dbd_1920.jpg', 'Family', 'As a Family Lawyer, you specialize in providing legal counsel on issues like divorce, child custody, and domestic violence. Responsibilities include case management, court representation, and mediation. Essential qualifications include a JD degree, bar admission, strong communication, negotiation skills, and empathy. The role involves guiding clients through legal processes, ensuring their rights are protected, and seeking favorable resolutions.', 20000),
-(34, 'copy img.jpg', 'Criminal Law', 'i am Criminal lawyer i am fro karachi ', 40000),
-(35, '', 'Labor Law', 'As a Personal Injury Lawyer, your primary responsibility is to represent individuals who have suffered harm due to accidents or negligence. Your tasks include conducting legal research, negotiating settlements, and representing clients in court. Key qualifications include a JD degree, bar admission, strong advocacy skills, and a deep understanding of personal injury law. Your role is crucial in helping clients seek compensation for injuries and navigating the complexities of the legal system to ensure fair outcomes.', 500000);
+(33, '../img/child-custody.jpeg', 'Family', 'As a Family Lawyer, you specialize in providing legal counsel on issues like divorce, child custody, and domestic violence. Responsibilities include case management, court representation, and mediation. Essential qualifications include a JD degree, bar admission, strong communication, negotiation skills, and empathy. The role involves guiding clients through legal processes, ensuring their rights are protected, and seeking favorable resolutions.', 20000),
+(34, '../img/avatar5.png', 'Criminal Law', ' \"Experience relentless advocacy and unwavering support with our Criminal Law Lawyer Services. At [Your Law Firm], our seasoned criminal defense attorneys are dedicated to protecting your rights and providing robust legal representation. Whether you are facing charges for offenses such as theft, assault, or white-collar crimes, our experts are here to guide you through the complexities of the legal system.', 76000),
+(35, '../img/avatar3.png', 'Labor Law', 'As a Personal Injury Lawyer, your primary responsibility is to represent individuals who have suffered harm due to accidents or negligence. Your tasks include conducting legal research, negotiating settlements, and representing clients in court. Key qualifications include a JD degree, bar admission, strong advocacy skills, and a deep understanding of personal injury law. Your role is crucial in helping clients seek compensation for injuries and navigating the complexities of the legal system to ensure fair outcomes.', 500000);
 
 -- --------------------------------------------------------
 
@@ -103,7 +111,7 @@ INSERT INTO `services` (`Id`, `Image`, `Servicetype`, `LawyerDesc`, `fees`) VALU
 CREATE TABLE `updateimage` (
   `Id` int(100) NOT NULL,
   `Name` varchar(100) NOT NULL,
-  `Desc` varchar(100) NOT NULL,
+  `Desc` varchar(1000) NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -112,10 +120,10 @@ CREATE TABLE `updateimage` (
 --
 
 INSERT INTO `updateimage` (`Id`, `Name`, `Desc`, `image`) VALUES
-(18, 'jack Michael', 'Jack Michael is a divorce lawyer he fought alot of big cases ', 'user8-128x128.jpg'),
-(19, 'Jeremy', 'jeremy is a criminal  lawyer he fought alot of big cases ', 'user1-128x128.jpg'),
-(20, 'Lorain martinez', 'Lorain martinez is a family lawyer she is very expereinced lawyer', 'user7-128x128.jpg'),
-(21, 'marry', 'marry is labor lawyer she is experienced her work', 'user4-128x128.jpg');
+(18, 'Selena Macron', 'Selena Macron information: Judith Susan Sheindlin, known professionally as Judge Judy, is an American ', '../img/lawyer2.jfif'),
+(19, 'nikhi chen', 'nikhi chen Information: Clooney is qualified to practice law in the United States, England, and Wale', '../img/46903ebb-17b6-44fe-84ec-09685ad5cf3c.jfif'),
+(21, 'marry', 'Marry holds a Juris Doctor (J.D.) degree, having graduated from [Law School Name]. She earned her law degree with honors, showcasing a deep understanding of legal principles and dedication to her studies.', '../img/e5ea2b90-193f-4d9d-a6d4-7db20cb380fd.jfif'),
+(22, 'Mark Geragos', 'Mark Geragos  information: Geragos was born in Los Angeles, California, where he attended Flintridge Preparatory School in La Cañada,', '../img/16a06956-8ac0-4a26-a532-762b24f8109c.jfif');
 
 -- --------------------------------------------------------
 
@@ -149,7 +157,9 @@ INSERT INTO `user-table` (`Id`, `Name`, `Email`, `Phone`, `Password`, `role_as`,
 (18, 'jawwadk', 'kcoder432@gmail.com', 74438733, 'abc', 0, NULL),
 (19, 'lyon', 'lyon@gmail.com', 2147483647, '123', 0, NULL),
 (20, 'malika', 'malika@gmail.com', 2147483647, '123', 0, NULL),
-(21, 'honeySingh', 'honey@gmail.com', 2147483647, '123', 0, NULL);
+(21, 'honeySingh', 'honey@gmail.com', 2147483647, '123', 0, NULL),
+(22, 'new', 'new@gmail.com', 2147483647, '321', 0, NULL),
+(23, 'max', 'max@gmail.com', 21474836, '1234', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -172,7 +182,28 @@ CREATE TABLE `user-table2` (
 INSERT INTO `user-table2` (`Id`, `Name`, `Email`, `Phone`, `Question`) VALUES
 (6, 'jawwad', 'jawwad@gmail.com', 2147483647, 'fsdafsdfsd'),
 (7, 'jawwad', 'jawwad@gmail.com', 2147483647, 'what is lawyer'),
-(8, 'jawwad', 'ab@gmail.com', 2147483647, 'what is the meaning of lawyer');
+(8, 'jawwad', 'ab@gmail.com', 2147483647, 'what is the meaning of lawyer'),
+(9, '', '', 0, ''),
+(10, '', '', 0, ''),
+(11, 'khan', 'jawwad@gmail.com', 21474836, 'sadasrdeqwrew'),
+(12, 'jawwad', 'ab@gmail.com', 21474836, 'dfsdfds'),
+(13, 'khan', 'ab@gmail.com', 21474836, 'dasds'),
+(14, '', '', 0, ''),
+(15, '', '', 0, ''),
+(16, '', '', 0, ''),
+(17, '', '', 0, ''),
+(18, '', '', 0, ''),
+(19, '', '', 0, ''),
+(20, '', '', 0, ''),
+(21, '', '', 0, ''),
+(22, '', '', 0, ''),
+(23, '', '', 0, ''),
+(24, 'jawwad', 'jawwad@gmail.com', 2147483647, 'sdda'),
+(25, 'jawwad', 'jawwadk638@gmail.com', 1234, 'dafsf'),
+(26, 'khan', 'jawwad@gmail.com', 2147483647, 'sdfsdfsd'),
+(27, 'khan', 'ab@gmail.com', 2147483647, 'saSFDSDFs'),
+(28, 'asAS', 'jawwadk638@gmail.com', 21474836, 'dasfdsfds'),
+(29, 'jawwad', 'jawwadk638@gmail.com', 2147483647, 'sdad3423dsfsd');
 
 --
 -- Indexes for dumped tables
@@ -222,13 +253,13 @@ ALTER TABLE `user-table2`
 -- AUTO_INCREMENT for table `appointlawyer`
 --
 ALTER TABLE `appointlawyer`
-  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `services`
@@ -240,19 +271,19 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT for table `updateimage`
 --
 ALTER TABLE `updateimage`
-  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user-table`
 --
 ALTER TABLE `user-table`
-  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `user-table2`
 --
 ALTER TABLE `user-table2`
-  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
